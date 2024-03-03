ce10e6a48220858bca304a907ed5dc0734abfb5f
BUSSO-coder <138984454+BUSSO-coder@users.noreply.github.com>
2024-03-03 20:20:14 +0100

    Create bxxo.is-an.app.js

---
+++ domains/bxxo.is-an.app.js
@@ -0,0 +1,10 @@
+addSubDomain({
+  description: 'an brawl stars api for my friends and me and all the people.', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'bxxo', // desired subdomain name
+  owner: {
+    repo: 'https://github.com/BUSSO-coder/bxxo.is-an.app/new/master/domains/bxxo.is-an.app.js',
+    email: 'mattibusso.social@gmail.com',
+  },
+  record: {
+    CNAME: 'BUSSO-coder.github.io', // e.g.: <your-github-account>.github.io
